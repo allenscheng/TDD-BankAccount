63ffc5d798591513910e1adc962d732381b52b7f
Allen Cheng <allencheng219@gmail.com>
2018-03-05 23:28:29 -0600

    added test to check for balance

--- bank_account_test.rb
+++ bank_account_test.rb
@@ -3,4 +3,9 @@ require_relative 'bank_account'
 
 class BankAccountTest < MiniTest::Unit::TestCase
 
+  def test_check_balance
+    account = BankAccount.new(100)
+    assert_equal "Your balance is $100", account.check_balance 
+  end
+  
 end
